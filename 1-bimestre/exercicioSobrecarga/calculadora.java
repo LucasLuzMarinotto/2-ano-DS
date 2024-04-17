75de8926c757132fe0af7b60d21a3f2a0ad43ba2
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2024-04-17 08:33:29 -0300

    Update calculadora.java

--- 1-bimestre/exercicioSobrecarga/calculadora.java
+++ 1-bimestre/exercicioSobrecarga/calculadora.java
@@ -3,22 +3,23 @@ package exercicio1;
 public class calculadora {
 	
 	private String nome;
-	private double resultado;
+	private double resultado;	
+	private int result;
 	
 	void setNome(String nom) {
 		this.nome = nom;
 	}
 	
-	void soma(int valor1, int valor2) {
-		this.resultado = valor1 + valor2;
+	void soma(int som1, int som2) {
+		this.result = som1 + som2;
 	}
 	
-	void soma(double valor1, double valor2) {
-		this.resultado = valor1 + valor2; 
+	void soma(double sum1, double sum2) {
+		this.resultado = sum1 + sum2; 
 	}
 	
-	void subtracao(int valor1, int valor2) {
-		this.resultado = valor1 - valor2;
+	void subtracao(int sub1, int sub2) {
+		this.result = sub1 - sub2;
 	}
 	
 	void subtracao(double valor1, double valor2) {
@@ -26,7 +27,13 @@ public class calculadora {
 	}
 	
 	String getResultado() {
-		return nome+" "+resultado;
+		if(resultado == 0){
+			return nome+" "+result;
+		}else if(result == 0) {
+			return nome+" "+resultado;
+		}else {
+			return nome+" "+resultado+" "+result;
+		}
 	}
 	
 }
