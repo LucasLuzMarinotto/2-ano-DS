388373b21d5816bf83eadd00041b61f0c055b8a0
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2024-05-28 08:48:09 -0300

    Update main.java

--- 2-bimestre/prova1/main.java
+++ 2-bimestre/prova1/main.java
@@ -1,1 +1,38 @@
+package questao1;
 
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		boolean decisao = false;
+		String resposta;
+		
+		livro livro1 = new livro();
+		revista revista1 = new revista();
+		
+		livro1.setTitulo("O Pequeno Príncipe");
+		livro1.setAutor("Antoine de Saint-Exupéry");
+		livro1.setAnoPublicacao(1943);
+		livro1.setNumeroPaginas(96);
+		livro1.setEditora("Harper Collins Brasil");
+		
+		revista1.setTitulo("Veja");
+		revista1.setAutor("Roberto Civita");
+		revista1.setAnoPublicacao(2024);
+		revista1.setMesPublicacao("Janeiro");
+		revista1.setNumeroEdicao(134);
+		
+		System.out.println("Você deseja ver os detalhes da revista Veja?");
+		resposta = in.next();
+		
+		if(resposta.equalsIgnoreCase("Sim")) {
+			
+		}
+	in.close();
+	}
+
+}
