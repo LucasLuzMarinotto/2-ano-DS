8962453556b1005e4539003ec15c1fcbd4d8c567
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2024-05-29 07:45:16 -0300

    Update main.java

--- 2-bimestre/atividadeHeranca/ex2/main.java
+++ 2-bimestre/atividadeHeranca/ex2/main.java
@@ -1,38 +1,17 @@
-package questao1;
-
-import java.util.Scanner;
+package exercicio2;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
+		cliente cliente1 = new cliente();
+		clientePremium clienteP1 = new clientePremium();
 		
-		boolean decisao = false;
-		String resposta;
+		cliente1.setValor_pedido(2000);
+		clienteP1.setValor_pedido(2000);
 		
-		livro livro1 = new livro();
-		revista revista1 = new revista();
-		
-		livro1.setTitulo("O Pequeno Príncipe");
-		livro1.setAutor("Antoine de Saint-Exupéry");
-		livro1.setAnoPublicacao(1943);
-		livro1.setNumeroPaginas(96);
-		livro1.setEditora("Harper Collins Brasil");
-		
-		revista1.setTitulo("Veja");
-		revista1.setAutor("Roberto Civita");
-		revista1.setAnoPublicacao(2024);
-		revista1.setMesPublicacao("Janeiro");
-		revista1.setNumeroEdicao(134);
-		
-		System.out.println("Você deseja ver os detalhes da revista Veja?");
-		resposta = in.next();
-		
-		if(resposta.equalsIgnoreCase("Sim")) {
-			
-		}
-	in.close();
+		System.out.println("O valor do desconto para o cliente normal é:"+cliente1.calcularDesconto());
+		System.out.println("O valor do desconto para o cliente premium é:"+clienteP1.calcularDesconto());
 	}
 
 }
