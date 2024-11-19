dfd82bd5e801dfc5133160cbcaed4b04c0939f5e
LucasLuzMarinotto <151086126+LucasLuzMarinotto@users.noreply.github.com>
2024-11-19 08:40:14 -0300

    Add files via upload

---
+++ 4-bimestre/interfaceGrafica/exemplo/programa_amigos/listarAmigos.java
@@ -0,0 +1,75 @@
+package programa_amigos;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JEditorPane;
+import javax.swing.SwingConstants;
+
+public class listarAmigos extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					listarAmigos frame = new listarAmigos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public listarAmigos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 759, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(216, 191, 216));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Lista de Amigos");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(269, 43, 163, 48);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Atualizar");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnNewButton.setBackground(new Color(135, 206, 250));
+		btnNewButton.setBounds(122, 323, 196, 64);
+		contentPane.add(btnNewButton);
+		
+		JButton btnDeletar = new JButton("Deletar");
+		btnDeletar.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnDeletar.setBackground(new Color(135, 206, 250));
+		btnDeletar.setBounds(432, 323, 180, 64);
+		contentPane.add(btnDeletar);
+		
+		JButton btnNewButton_1 = new JButton("<");
+		btnNewButton_1.setBackground(new Color(135, 206, 250));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_1.setBounds(44, 19, 89, 89);
+		contentPane.add(btnNewButton_1);
+	}
+}
